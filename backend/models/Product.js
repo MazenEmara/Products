d0f15a50bcaeb9412fdb2293a0034a44af13e723
Mazen Emara <mazenemara@Mazens-MacBook-Pro.local>
2022-06-03 22:07:53 +0200

    rrr

---
+++ backend/models/Product.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const ProductSchema = new Schema({
+    Product_name : {
+        type : String,
+        required : true,
+        unique:true
+    },
+    Price : Number,
+    weight : String,
+    description : String,
+    quantity : { 
+        type : Number,
+        default : 0
+    },
+    //orderdproducts: [{
+        //type: Schema.Types.ObjectId,
+        //ref: 'Product'
+     //}//]
+},{collection: "product"});
+module.exports = Product = mongoose.model('product',ProductSchema);
